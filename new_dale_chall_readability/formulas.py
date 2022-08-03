fad1e4cd0c16570b6e9b5b494c6dbccf20f8ae12
Robb Shecter <robb@public.law>
2022-08-03 17:04:12 -0600

    tests pass

--- new_dale_chall_readability/formulas.py
+++ new_dale_chall_readability/formulas.py
@@ -1,4 +1,28 @@
-def cloze(pct_unfamiliar_words: float, avg_sentence_length: float) -> float:
+from typing import Literal, TypeAlias
+
+
+ReadingLevel: TypeAlias = Literal[
+    "1", "2", "3", "4", "5-6", "7-8", "9-10", "11-12", "13-15", "16+"
+]
+
+"""
+See Table 5-8, p. 74.
+"""
+EQUIV_CLOZE_AND_READING_LEVELS: dict[tuple[int | None, int | None], ReadingLevel] = {
+    (57, None): "1",
+    (53, 57): "2",
+    (49, 53): "3",
+    (44, 49): "4",
+    (39, 44): "5-6",
+    (33, 39): "7-8",
+    (27, 33): "9-10",
+    (21, 27): "11-12",
+    (15, 21): "13-15",
+    (None, 15): "16+",
+}
+
+
+def cloze_score(pct_unfamiliar_words: float, avg_sentence_length: float) -> float:
     """
     Compute the Cloze score (Chall & Dale, 1995, p.66).
     Table 2-1 shows cloze scores rounded to two decimal places. This
@@ -7,3 +31,12 @@ def cloze(pct_unfamiliar_words: float, avg_sentence_length: float) -> float:
     raw_result = 64 - (95 * pct_unfamiliar_words) - (0.69 * avg_sentence_length)
 
     return round(raw_result, 2)
+
+
+def reading_level_from_cloze(cloze_score: float) -> ReadingLevel:
+    """
+    Compute the reading level from the Cloze score.
+    """
+    for keys in EQUIV_CLOZE_AND_READING_LEVELS:
+        if cloze_score > keys[0] and cloze_score <= keys[1]:
+            return EQUIV_CLOZE_AND_READING_LEVELS[keys]
--- tests/new_dale_chall_readability_test.py
+++ tests/new_dale_chall_readability_test.py
@@ -1,15 +1,23 @@
-from new_dale_chall_readability.formulas import cloze
+from new_dale_chall_readability.formulas import cloze_score, reading_level_from_cloze
 
 
 def test_with_round_numbers():
     """
     Test case extracted from Chall and Dale (1995, page 31, Table 2.1).
     """
-    assert cloze(pct_unfamiliar_words=0.2, avg_sentence_length=10.0) == 38.1
+    assert cloze_score(pct_unfamiliar_words=0.2, avg_sentence_length=10.0) == 38.1
 
 
 def test_a_higher_result():
     """
     Test case extracted from Chall and Dale (1995, page 33, Table 2.1).
     """
-    assert cloze(pct_unfamiliar_words=0.11, avg_sentence_length=100 / 27) == 50.99
+    assert cloze_score(pct_unfamiliar_words=0.11, avg_sentence_length=100 / 27) == 50.99
+
+
+class TestReadingLevelFromCloze:
+    def test_a_lowest_bound(self):
+        assert reading_level_from_cloze(39.01) == "5-6"
+
+    def test_the_upper_bound(self):
+        assert reading_level_from_cloze(44.0) == "5-6"
