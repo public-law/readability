03e08a2044a69cbb1e9b9f2b1eb893f1098fe444
Robb Shecter <robb@public.law>
2022-08-03 16:38:10 -0600

    tests pass

--- new_dale_chall_readability/formulas.py
+++ new_dale_chall_readability/formulas.py
@@ -1,5 +1,9 @@
 def cloze(pct_unfamiliar_words: float, avg_sentence_length: float) -> float:
     """
     Compute the Cloze score (Chall & Dale, 1995, p.66).
+    Table 2-1 shows cloze scores rounded to two decimal places. This
+    function follows suit.
     """
-    return 64 - (95 * pct_unfamiliar_words) - (0.69 * avg_sentence_length)
+    raw_result = 64 - (95 * pct_unfamiliar_words) - (0.69 * avg_sentence_length)
+
+    return round(raw_result, 2)
