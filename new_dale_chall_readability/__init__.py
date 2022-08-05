04573021f246ad93d7a5eab181891aabfc3b5161
Robb Shecter <robb@public.law>
2022-08-04 21:44:01 -0600

    refactor: extract function avg_sentence_length

--- new_dale_chall_readability/__init__.py
+++ new_dale_chall_readability/__init__.py
@@ -1,14 +1,11 @@
-import re
-
-from .utils import pct_unfamiliar_words
-from .formulas import (ReadingLevel, compute_cloze_score,
-                       reading_level_from_cloze)
+from .utils import avg_sentence_length, pct_unfamiliar_words
+from .formulas import ReadingLevel, compute_cloze_score, reading_level_from_cloze
 
 # Compensate for the inaccurate easy word search.
 # In a nutshell, the current code under-counts the number
 # of easy words. A TODO is to get the easy word search
 # closer to the specification.
-_COMPENSATION_FACTOR_1 = 1.349   # For lower cloze scores
+_COMPENSATION_FACTOR_1 = 1.349  # For lower cloze scores
 _COMPENSATION_FACTOR_2 = 1.2315  # For higher cloze scores
 
 
@@ -16,17 +13,10 @@ def cloze_score(text: str) -> float:
     """
     Calculate the text's cloze score.
     """
-    cleaned_up_text = text.replace("\n", " ").strip()
-    sentences = re.findall(r"\b[^.!?]+[.!?]*", cleaned_up_text, re.UNICODE)
-    words = [w.lower().strip('.(),"') for w in text.split()]
-
-    avg_sentence_len = len(words) / len(sentences)
-
     raw_score = compute_cloze_score(
         pct_unfamiliar_words=pct_unfamiliar_words(text),
-        avg_sentence_length=avg_sentence_len,
+        avg_sentence_length=avg_sentence_length(text),
     )
-
     compensation_factor = (
         _COMPENSATION_FACTOR_1 if raw_score < 40 else _COMPENSATION_FACTOR_2
     )
--- new_dale_chall_readability/utils.py
+++ new_dale_chall_readability/utils.py
@@ -1,3 +1,4 @@
+import re
 from .easy_words import EASY_WORDS as _EASY_WORDS
 
 
@@ -7,3 +8,11 @@ def pct_unfamiliar_words(text: str) -> float:
     unfamiliar_words = [w for w in no_possesives if w not in _EASY_WORDS]
 
     return len(unfamiliar_words) / len(words)
+
+
+def avg_sentence_length(text: str) -> float:
+    cleaned_up_text = text.replace("\n", " ").strip()
+    sentences = re.findall(r"\b[^.!?]+[.!?]*", cleaned_up_text, re.UNICODE)
+    words = [w.lower().strip('.(),"') for w in text.split()]
+
+    return len(words) / len(sentences)
