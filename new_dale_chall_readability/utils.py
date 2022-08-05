9652485e0c8e5ff0eb63952005ab298959fe9496
Robb Shecter <robb@public.law>
2022-08-04 21:39:27 -0600

    refactor: generator

--- new_dale_chall_readability/utils.py
+++ new_dale_chall_readability/utils.py
@@ -3,7 +3,7 @@ from .easy_words import EASY_WORDS as _EASY_WORDS
 
 def pct_unfamiliar_words(text: str) -> float:
     words = [w.lower().strip('.(),"') for w in text.split()]
-    no_possesive_words = [w.replace("'s", "").replace("s'", "") for w in words]
-    unfamiliar_words = [w for w in no_possesive_words if w not in _EASY_WORDS]
+    no_possesives = (w.replace("'s", "").replace("s'", "") for w in words)
+    unfamiliar_words = [w for w in no_possesives if w not in _EASY_WORDS]
 
     return len(unfamiliar_words) / len(words)
