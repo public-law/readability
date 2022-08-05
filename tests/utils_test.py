5c47882c0a464a80c0af8ce7eb6386ea38c2e23f
Robb Shecter <robb@public.law>
2022-08-04 21:19:49 -0600

    refactor: tests

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,23 +1,23 @@
 from new_dale_chall_readability.utils import pct_unfamiliar_words
 
 
-class DescribePctUnfamiliarWords:
-    def it_handles_all_familiar(self):
+class TestPctUnfamiliarWords:
+    def test_all_familiar(self):
         input = "Dog cat play house."
 
         assert pct_unfamiliar_words(input) == 0.0
 
-    def it_handles_repeated_uses(self):
+    def test_repeated_uses(self):
         input = "Dog respondent dog respondent dog respondent."
 
         assert pct_unfamiliar_words(input) == 0.5
 
-    def it_handles_mixed_uses(self):
+    def test_mixed_uses(self):
         input = "Dog raconteur dog respondent dog respondent."
 
         assert pct_unfamiliar_words(input) == 0.5
 
-    def it_handles_all_unfamiliar(self):
+    def test_all_unfamiliar(self):
         input = "Deponent deponent deponent."
 
         assert pct_unfamiliar_words(input) == 1.0
