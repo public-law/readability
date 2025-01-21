8aa98ef6ed4d49a9a8779e0274e77428e5216884
Connor Meeker <123894581+ConnorWM97@users.noreply.github.com>
2025-01-20 19:24:05 -0500

    Update utils.py
    added more documentation

--- new_dale_chall_readability/utils.py
+++ new_dale_chall_readability/utils.py
@@ -10,6 +10,14 @@ warnings.filterwarnings("ignore", category=UserWarning, module="bs4")
 
 
 def pct_unfamiliar_words(text: str) -> float:
+    """
+    Calculates the percentage of unfamiliar words in a string.
+
+    step1: calls all words in a tuple from _words.
+    step2: removes the possesives from any words in said tuple.
+    step3: counts the number of unfamiliar words from cleaned tuple using _is_unfamiliar.
+    step3: returns the percentage by dividing the length of unfamiliar words by words.
+    """
     words = _words(text)
     no_possesives = (w.replace("'s", "").replace("s'", "") for w in words)
     unfamiliar_words = [w for w in no_possesives if _is_unfamiliar(w)]
@@ -18,6 +26,16 @@ def pct_unfamiliar_words(text: str) -> float:
 
 
 def avg_sentence_length(text: str) -> float:
+    """
+    Calculates average number of words per sentence in text.
+
+    step1: remove newlines and trailing white space from text.
+    step2: seperates text into sentances based on non-punctuation characters
+    followed by an optional punctuation
+    step3: calls _words to return all words in a tuple
+    step4: returns the average length by dividing the length of words to the length
+    of sentences.
+    """
     cleaned_up_text = text.replace("\n", " ").strip()
     sentences = re.findall(r"\b[^.!?]+[.!?]*", cleaned_up_text, re.UNICODE)
     #prevents division of zero error
@@ -29,6 +47,21 @@ def avg_sentence_length(text: str) -> float:
 
 
 def _words(in_text: str) -> tuple[str, ...]:
+    """
+    Takes the plain text that is entered and returns a tuple of words.
+
+    Args:
+        in_text: an string of words that includes HTML tags, punctuation marks, and capital words.
+        otherwise known as plain_text.
+    Returns:
+        tuple: a tuple of individual words that are stripped of HTML tags, punctuation marks, 
+        and turns Capital letters into lower case.
+
+    Example:
+        text = <p>The excited child ran to the park, yelling, 'Look, a big red ball!'
+        as she chased after it, laughing with glee.</p>
+        words = _words(text) # ("the", "excited", "child", etc...)
+    """
     plain_text = BeautifulSoup(in_text, "html.parser").text
 
     return tuple(w.lower().strip('.(),"') for w in plain_text.split())
